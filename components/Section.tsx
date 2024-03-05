e8dcddb06f3a42c2646b40fcb2f169c647daae64
xSpookyDev <ricardo13dev@gmail.com>
2024-03-04 23:18:31 -0300

    Practica objetos

---
+++ components/Section.tsx
@@ -0,0 +1,9 @@
+
+
+const Section = () => {
+  return (
+    <div>Section</div>
+  )
+}
+
+export default Section
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,60 +1,71 @@
 import {useEffect } from "react"
-import {Scene,
-WebGL1Renderer, PerspectiveCamera, Mesh, MeshBasicMaterial, BoxGeometry,SphereGeometry, TextureLoader, BackSide, MeshPhongMaterial,DirectionalLight, HemisphereLight, AmbientLight
-} from "three"
+import Background from "../components/Background"
+
+class Humano{
+
+    public nombre : string
+    public altura : number
+    public peso : number
+
+    constructor(nombre:string, altura:number, peso:number){
+       // console.log("hola soy un humano: "+ this.nombre)
+       this.nombre = nombre;
+       
+       this.altura = altura;
+       this.peso = peso;
+       this.saludar()
+    }
+
+    private saludar(){
+        console.log(`Mi nombre es ${this.nombre}`)
+    }
+}
+
+class Peleador extends Humano{
+    private poder = 100
+    private defensa = 100
+    
+    constructor(nombre, scene:Scene){
+    super(nombre, 1.42, 50)
+    console.log(this.nombre)
+    }
+
+    public duplicarEstadisticas(){
+        this.poder *= 2
+        this.defensa *=2
+        console.log(this.poder)
+        console.log(this.defensa)
+    }
+
+    public Ejecutar(fn){
+        fn()
+    }
+}
+
+class Scene{
+    private score = 0
+
+
+    constructor(){
+      const peleador = new Peleador("nombre",this)
+      const persona = new Humano("Ricardo", 1.80,60)
+
+     // peleador.Ejecutar(this.aumentarScore.bind(this))
+    }
+
+    private aumentarScore(){
+        this.score +=10
+      console.log(this)
+    }
+}
 
 
 function HomePage(){
-    useEffect(()=>{
-        const scene = new Scene()
-        const rederer = new WebGL1Renderer({
-            antialias:true,
-            canvas: document.getElementById("bg")
-        })
-        
-        const camara = new PerspectiveCamera(70, window.innerWidth/window.innerHeight, 0.1,1000)
-
-        //mover camara 
-        camara.position.z = 6;
-
-
-
-        //crear cubo
-        const geometria = new BoxGeometry(1,1,1)
-        const material = new MeshBasicMaterial({color: 0xffffff })
-        const cubo = new Mesh(geometria, material)
-        scene.add(cubo)
-
-
-        //crear skybox
-        const skyGeometry = new SphereGeometry(360, 25, 25)
-        const loader = new TextureLoader()
-        const textura = loader.load("/custom-sky.png")
-        const material2 = new MeshPhongMaterial({
-            map: textura
-        })
-        const skybox = new Mesh(skyGeometry,material2)
-        scene.add(skybox);
-        skybox.material.side= BackSide
-
-
-        //crear iluminacion
-        scene.add(new AmbientLight(0xffffff, 0.8))
-        scene.add(new HemisphereLight(0xffffff, 0.8))
-
-        rederer.setSize(window.innerWidth, window.innerHeight)
-
-
-        function animate(){
-            cubo.rotation.x +=0.02
-            cubo.rotation.y +=0.02
-            rederer.render(scene, camara)
-            requestAnimationFrame(animate)
-        }
-        animate();
-    },[])
-
-    return <canvas id="bg"/>
+    new Scene()
+    const peleador = new Peleador("Lenin",this)
+    peleador.duplicarEstadisticas()
+    
+    return <Background/>
 }
 
 export default HomePage
